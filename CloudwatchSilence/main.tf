2c2a51b717e2322ccfcd65105bec6cc0dffc74ab
Andres <arenasandres7@gmail.com>
2026-01-30 14:20:36 -0500

    update15

--- CloudwatchSilence/main.tf
+++ CloudwatchSilence/main.tf
@@ -1,122 +1,11 @@
-# resource "aws_ssm_document" "mute_alarms" {
-#   name          = "mute-cloudwatch-alarms"
-#   document_type = "Automation"
-
-#   content = jsonencode({
-#     schemaVersion = "0.3"
-#     assumeRole    = aws_iam_role.ssm_automation_role.arn
-
-#     mainSteps = [{
-#       name   = "DisableAlarms"
-#       action = "aws:executeAwsApi"
-#       inputs = {
-#         Service    = "CloudWatch"
-#         Api        = "DisableAlarmActions"
-#         AlarmNames = var.alarm_names
-#       }
-#     }]
-#   })
-# }
-
-
-# resource "aws_ssm_document" "unmute_alarms" {
-#   name          = "unmute-cloudwatch-alarms"
-#   document_type = "Automation"
-
-#   content = jsonencode({
-#     schemaVersion = "0.3"
-#     assumeRole    = aws_iam_role.ssm_automation_role.arn
-
-#     mainSteps = [{
-#       name   = "EnableAlarms"
-#       action = "aws:executeAwsApi"
-#       inputs = {
-#         Service    = "CloudWatch"
-#         Api        = "EnableAlarmActions"
-#         AlarmNames = var.alarm_names
-#       }
-#     }]
-#   })
-# }
-
-# resource "aws_iam_role" "ssm_automation_role" {
-#   name        = "${var.name_prefix}-ssm-automation-role"
-#   description = "IAM Role for SSM Automation to mute/unmute CloudWatch Alarms"
-
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [{
-#       Effect = "Allow"
-#       Principal = {
-#         Service = "ssm.amazonaws.com"
-#       }
-#       Action = "sts:AssumeRole"
-#     }]
-#   })
-
-# }
-
-# resource "aws_iam_policy" "ssm_automation_policy" {
-#   name        = "${var.name_prefix}-ssm-automation-role-policy"
-#   description = "Policy for SSM Automation to mute/unmute CloudWatch Alarms"
-
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [{
-#       Effect = "Allow"
-#       Action = [
-#         "cloudwatch:DisableAlarmActions",
-#         "cloudwatch:EnableAlarmActions"
-#       ]
-#       Resource = "*"
-#     }]
-#   })
-
-# }
-
-
-# resource "aws_cloudwatch_event_target" "mute_target" {
-#   rule = aws_cloudwatch_event_rule.mute.name
-#   arn  = aws_ssm_document.mute_alarms.arn
-
-#   role_arn = aws_iam_role.eventbridge_role_assume.arn
-
-#   input = jsonencode({
-#     DocumentName = aws_ssm_document.mute_alarms.name
-#     Parameters = {
-#       AutomationAssumeRole = [aws_iam_role.ssm_automation_role.arn]
-#     }
-#   })
-# }
-
-# resource "aws_cloudwatch_event_target" "unmute_target" {
-#   rule = aws_cloudwatch_event_rule.unmute.name
-#   arn  = aws_ssm_document.unmute_alarms.arn
-
-#   role_arn = aws_iam_role.eventbridge_role_assume.arn
-
-#   input = jsonencode({
-#     DocumentName = aws_ssm_document.mute_alarms.name
-#     Parameters = {
-#       AutomationAssumeRole = [aws_iam_role.ssm_automation_role.arn]
-#     }
-#   })
-
-#   depends_on = [aws_iam_role_policy.eventbridge_ssm_policy]
-# }
-
-# resource "aws_iam_role_policy_attachment" "ssm_automation_role_attachment" {
-#   role       = aws_iam_role.ssm_automation_role.name
-#   policy_arn = aws_iam_policy.ssm_automation_policy.arn
-# }
-
-
 resource "aws_ssm_document" "mute_alarms" {
   name          = "mute-cloudwatch-alarms"
   document_type = "Automation"
 
   content = jsonencode({
     schemaVersion = "0.3"
+    assumeRole    = aws_iam_role.ssm_automation_role.arn
+
     mainSteps = [{
       name   = "DisableAlarms"
       action = "aws:executeAwsApi"
@@ -129,12 +18,15 @@ resource "aws_ssm_document" "mute_alarms" {
   })
 }
 
+
 resource "aws_ssm_document" "unmute_alarms" {
   name          = "unmute-cloudwatch-alarms"
   document_type = "Automation"
 
   content = jsonencode({
     schemaVersion = "0.3"
+    assumeRole    = aws_iam_role.ssm_automation_role.arn
+
     mainSteps = [{
       name   = "EnableAlarms"
       action = "aws:executeAwsApi"
@@ -147,12 +39,46 @@ resource "aws_ssm_document" "unmute_alarms" {
   })
 }
 
+resource "aws_iam_role" "ssm_automation_role" {
+  name        = "${var.name_prefix}-ssm-automation-role"
+  description = "IAM Role for SSM Automation to mute/unmute CloudWatch Alarms"
 
-data "aws_caller_identity" "current" {}
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ssm.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+
+}
+
+resource "aws_iam_policy" "ssm_automation_policy" {
+  name        = "${var.name_prefix}-ssm-automation-role-policy"
+  description = "Policy for SSM Automation to mute/unmute CloudWatch Alarms"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "cloudwatch:DisableAlarmActions",
+        "cloudwatch:EnableAlarmActions"
+      ]
+      Resource = "*"
+    }]
+  })
+
+}
+
 
 resource "aws_cloudwatch_event_target" "mute_target" {
-  rule     = aws_cloudwatch_event_rule.mute.name
-  arn      = "arn:aws:ssm:${var.region}:${data.aws_caller_identity.current.account_id}:automation-definition/${aws_ssm_document.mute_alarms.name}"
+  rule = aws_cloudwatch_event_rule.mute.name
+  arn  = aws_ssm_document.mute_alarms.arn
+
   role_arn = aws_iam_role.eventbridge_role_assume.arn
 
   input = jsonencode({
@@ -164,15 +90,23 @@ resource "aws_cloudwatch_event_target" "mute_target" {
 }
 
 resource "aws_cloudwatch_event_target" "unmute_target" {
-  rule     = aws_cloudwatch_event_rule.unmute.name
-  arn      = "arn:aws:ssm:${var.region}:${data.aws_caller_identity.current.account_id}:automation-definition/${aws_ssm_document.unmute_alarms.name}"
+  rule = aws_cloudwatch_event_rule.unmute.name
+  arn  = aws_ssm_document.unmute_alarms.arn
+
   role_arn = aws_iam_role.eventbridge_role_assume.arn
 
   input = jsonencode({
-    DocumentName = aws_ssm_document.unmute_alarms.name
+    DocumentName = aws_ssm_document.mute_alarms.name
     Parameters = {
       AutomationAssumeRole = [aws_iam_role.ssm_automation_role.arn]
     }
   })
+
+  depends_on = [aws_iam_role_policy.eventbridge_ssm_policy]
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_automation_role_attachment" {
+  role       = aws_iam_role.ssm_automation_role.name
+  policy_arn = aws_iam_policy.ssm_automation_policy.arn
 }
 
--- CloudwatchSilence/variables.tf
+++ CloudwatchSilence/variables.tf
@@ -24,9 +24,3 @@ variable "unmute_cron" {
   type        = string
 }
 
-
-variable "region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-1"
-}
